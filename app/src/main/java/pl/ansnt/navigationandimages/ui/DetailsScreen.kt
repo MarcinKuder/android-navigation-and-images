152614f6d3c421a0aa4c5405faace273183d28e5
Marcin Kuder <marcin.kuder@gmail.com>
2024-12-10 11:04:37 +0100

    images

--- app/src/main/java/pl/ansnt/navigationandimages/ui/DetailsScreen.kt
+++ app/src/main/java/pl/ansnt/navigationandimages/ui/DetailsScreen.kt
@@ -1,10 +1,45 @@
 package pl.ansnt.navigationandimages.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun DetailsScreen(modifier: Modifier = Modifier, imageId: Int, onOpenURL: (String) -> Unit) {
-    Text("Details of $imageId")
+    val imgWidth = 800
+    val imgHeight = 600
+    val url = "https://picsum.photos/seed/$imageId/$imgWidth/$imgHeight"
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(32.dp)
+    ) {
+        AsyncImage(
+            model = url,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(imgWidth / imgHeight.toFloat())
+                .clip(MaterialTheme.shapes.medium),
+            contentDescription = "Random image",
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton ( onClick = {
+            onOpenURL(url) }){
+            Text("Open in browser")
+        }
+    }
 }
--- app/src/main/java/pl/ansnt/navigationandimages/ui/GridScreen.kt
+++ app/src/main/java/pl/ansnt/navigationandimages/ui/GridScreen.kt
@@ -1,20 +1,45 @@
 package pl.ansnt.navigationandimages.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun GridScreen(modifier: Modifier = Modifier, onImageClick: (Int) -> Unit) {
-    TextButton (
-        onClick = { onImageClick(1) },
-        shape = RectangleShape,
-        contentPadding = PaddingValues(0.dp),
+    val imgWidth = 800
+    val imgHeight = 600
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Adaptive(150.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        Text("Details")
+        items(32) { imageId ->
+            val imgUrl = "https://picsum.photos/seed/$imageId/$imgWidth/$imgHeight"
+            TextButton (
+                onClick = { onImageClick(imageId) },
+                shape = RectangleShape,
+                contentPadding = PaddingValues(0.dp),
+            ) {
+                AsyncImage(
+                    model = imgUrl,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(imgWidth / imgHeight.toFloat())
+                        .wrapContentHeight()
+                )
+            }
+        }
     }
 }
